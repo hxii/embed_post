533be9e647ab7da1d2379ae820718ae2a61e6c57
Paul (hxii) <paul@glushak.net>
2019-11-27 12:25:39 +0200

    Update to 1.0.1
    You can now specify a class in case you want to style each embed.

--- class-hxii-embed-post.php
+++ class-hxii-embed-post.php
@@ -8,7 +8,7 @@
  * Plugin Name:       HXII Embed Post
  * Plugin URI:        https://github.com/hxii/embed_post
  * Description:       Allows you to embed posts in other posts for easy reference.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            hxii
  * Author URI:        https://paulglushak.com
  * License:           GPL-2.0+
@@ -23,6 +23,9 @@ class HXII_Embed_Post {
 
 	/**
 	 * Actions etc
+	 *
+	 * @since 1.0.0
+	 * @return void
 	 */
 	public function __construct() {
 		add_shortcode( 'embed_post', array( $this, 'embed_post' ) );
@@ -33,6 +36,7 @@ class HXII_Embed_Post {
 	 * Get post by ID and return content.
 	 *
 	 * @param array $args shortcode arguments.
+	 * @since 1.0.0
 	 * @return string
 	 */
 	public function embed_post( $args ) {
@@ -41,8 +45,9 @@ class HXII_Embed_Post {
 			if ( ! is_null( $post ) ) {
 				$title     = $post->post_title;
 				$permalink = get_permalink( $post );
+				$class     = ( isset( $args['class'] ) ) ? ' ' . $args['class'] : ' default';
 				$output    = apply_filters( 'the_content', $post->post_content );
-				return "<div class=\"embed_post\"><div class=\"embed_content\">$output</div>
+				return "<div class=\"embed_post{$class}\"><div class=\"embed_content\">$output</div>
 				<div class=\"embed_meta\">From <a href=\"$permalink\">$title</a> &ndash; Modified {$post->post_modified}</div>
 				</div>";
 			}
@@ -51,21 +56,32 @@ class HXII_Embed_Post {
 
 	/**
 	 * Inline stylesheet.
+	 *
+	 * @since 1.0.0
+	 * @return void
 	 */
 	public function style() {
 		echo '<style>
-		.embed_post {
+		.embed_post.default {
 			display: flex;
 			flex-direction: column;
 			padding: .5em;
 			border: 1px solid #eee;
-			background: #fdfdfd;
+			background: #eee;
 		}
-		.embed_meta {
+		.embed_post.default .embed_meta {
 			align-self: flex-end;
 			font-size: .75em;
 			text-transform: uppercase;
 			font-weight: 600;
+		}
+		.embed_post.seamless {
+			background: transparent;
+			border: none;
+			padding: 0;
+		}
+		.embed_post.seamless .embed_meta {
+			display: none;
 		}
 		</style>';
 	}
